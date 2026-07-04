b9c1b1345fd39ff66f144861002a7f34e164448d
happybaggy <94029132+happybaggy@users.noreply.github.com>
2026-07-04 08:29:57 +0000

    HAVOCFARM

--- havocfarm.lua
+++ havocfarm.lua
@@ -1,3 +1,5 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
